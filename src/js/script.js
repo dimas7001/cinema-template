2417809e36039b49c8ae94195b51f8c1789284c3
Dmytro Baranov <ba.dm.ig.bdi@gmail.com>
2021-11-24 11:22:43 +0200

    search: done

--- src/js/script.js
+++ src/js/script.js
@@ -22,3 +22,24 @@ $('.nav .dropdown__list').click((e) =>
 $('.place:not(.place_disabled):not(.place_taken)').click((e) => {
     $(e.target).toggleClass('place_active');
 })
+
+let filmNames = [];
+let movies = [];
+if ($('.input__search')) {
+    movies = $('.section__films a');
+    movies.each((i, e) => filmNames[i] = e.dataset.name.toLowerCase());
+}
+$('.input__search').on('input', (e) => {
+    $('.section__films a[data-flag="1"]').each((i, e_r) => {
+        e_r.style.display = 'block';
+        e_r.dataset.flag = 0;
+    })
+    let value = $(e.target).val().toLowerCase();
+    if (value.length >= 2)
+        $.each(filmNames, (i, f) => {
+            if (f.indexOf(value) === -1) {
+                movies[i].style.display = 'none';
+                movies[i].dataset.flag = 1;
+            }
+        })
+})
